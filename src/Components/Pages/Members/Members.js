ec95bb3a6b67df198ce0164f5ec04ba142fd4bc1
shreyash7865 <shreyashpasi@gmail.com>
2023-07-08 08:36:37 +0530

    member-signup commit

--- src/Components/Pages/Members/Members.js
+++ src/Components/Pages/Members/Members.js
@@ -1,20 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Members() {
+const Members = () => {
+  const [fName, setFName] = useState('');
+  const [lName, setLName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [address, setAddress] = useState('');
+
+  const handleSignUp = () => {
+    // Make a POST request to your backend endpoint for sign up
+    fetch('https://ngo-backend-wz9s.onrender.com/api/donor/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        f_name: fName,
+        l_name: lName,
+        email: email,
+        password: password,
+        address: address,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response from the server
+        console.log(data);
+        // Reset the form
+        setFName('');
+        setLName('');
+        setEmail('');
+        setPassword('');
+        setAddress('');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
   return (
-    <div className='ngo-request-section'>
-        <div className="ngo-request-form">
-            <form action="">
-                <input type="text" placeholder='Enter Name' /><br />
-                <input type="text" placeholder='Enter Your Email' /> <br />
-                <input type="text" placeholder='Enter Password' /> <br />
-                <input type="text" placeholder='Confirm Password' /> <br />
-                <button>Submit</button>
-
-            </form>
-        </div>
+    <div>
+      <h2>Member Sign Up</h2>
+      <form>
+        <label>
+          First Name:
+          <input
+            type="text"
+            value={fName}
+            onChange={(e) => setFName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            value={lName}
+            onChange={(e) => setLName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Address:
+          <input
+            type="text"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleSignUp}>
+          Sign Up
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Members
+export default Members;
